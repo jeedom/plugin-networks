3ba525300741c025066c0605f0a847fd7423fceb
loic <lgevrey@oxya.com>
2015-11-13 11:24:08 +0100

    Add panel

---
+++ core/ajax/networks.ajax.php
@@ -0,0 +1,65 @@
+<?php
+
+/* This file is part of Jeedom.
+ *
+ * Jeedom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jeedom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+try {
+	require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+	include_file('core', 'authentification', 'php');
+
+	if (!isConnect('admin')) {
+		throw new Exception(__('401 - Accès non autorisé', __FILE__));
+	}
+
+	if (init('action') == 'getNetworks') {
+		if (init('object_id') == '') {
+			$object = object::byId($_SESSION['user']->getOptions('defaultDashboardObject'));
+		} else {
+			$object = object::byId(init('object_id'));
+		}
+		if (!is_object($object)) {
+			$object = object::rootObject();
+		}
+		$return = array();
+		$return['eqLogics'] = array();
+		if (init('object_id') == '') {
+			foreach (object::all() as $object) {
+				foreach ($object->getEqLogic(true, false, 'networks') as $networks) {
+					$return['eqLogics'][] = $networks->toHtml(init('version'));
+				}
+			}
+		} else {
+			foreach ($object->getEqLogic(true, false, 'networks') as $networks) {
+				$return['eqLogics'][] = $networks->toHtml(init('version'));
+			}
+			foreach (object::buildTree($object) as $child) {
+				$cameras = $child->getEqLogic(true, false, 'networks');
+				if (count($cameras) > 0) {
+					foreach ($cameras as $networks) {
+						$return['eqLogics'][] = $networks->toHtml(init('version'));
+					}
+				}
+			}
+		}
+		ajax::success($return);
+	}
+
+	throw new Exception(__('Aucune methode correspondante à : ', __FILE__) . init('action'));
+	/*     * *********Catch exeption*************** */
+} catch (Exception $e) {
+	ajax::error(displayExeption($e), $e->getCode());
+}
+?>

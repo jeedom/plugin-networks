7cae30849d81786cd2ff2a477e0b2a0443bcb337
Loïc <loic@jeedom.com>
2017-08-07 15:45:10 +0200

    Update lib

--- vendor/tomnomnom/phpwol/Phpwol/MagicPacket.php
+++ vendor/tomnomnom/phpwol/Phpwol/MagicPacket.php
@@ -5,9 +5,10 @@ class MagicPacket {
   protected $socket;
   protected $lastError = 0;
 
-  const ERR_INVALID_IP     = 1;
-  const ERR_INVALID_MAC    = 2;
-  const ERR_INVALID_SUBNET = 4;
+  const ERR_INVALID_IP         = 1;
+  const ERR_INVALID_MAC        = 2;
+  const ERR_INVALID_SUBNET     = 4;
+  const ERR_SOCKET_SEND_FAILED = 8;
 
   public function __construct(Socket $socket = null){
     $this->socket = $socket;
@@ -17,10 +18,20 @@ class MagicPacket {
     // Reset the last error
     $this->lastError = 0;
 
+    if (!filter_var($ip, FILTER_VALIDATE_IP)){
+      $this->lastError = self::ERR_INVALID_IP;
+      return false;
+    }
+
     // If we're not given a subnet assume a broadcast IP
     if (is_null($subnet)){
       $broadcastIP = $ip;
     } else {
+
+      if (!filter_var($subnet, FILTER_VALIDATE_IP)){
+        $this->lastError = self::ERR_INVALID_SUBNET;
+        return false;
+      }
       $broadcastIP = $this->getBroadcastIP($ip, $subnet);
     }
 
@@ -31,17 +42,17 @@ class MagicPacket {
       return false;
     }
 
-    if (!filter_var($broadcastIP, FILTER_VALIDATE_IP)){
-      $this->lastError = self::ERR_INVALID_IP;
-      return false;
-    }
     
     $binMac = pack('H12', $hexMac);
     $prefix = pack('H12', str_repeat('FF', 6));
 
     $magicPacket = $prefix . str_repeat($binMac, 16);
     
-    $this->socket->sendBroadcastUDP($magicPacket, $broadcastIP, 7);
+    $sent = $this->socket->sendBroadcastUDP($magicPacket, $broadcastIP, 7);
+    if (!$sent){
+      $this->lastError = self::ERR_SOCKET_SEND_FAILED;
+      return false;
+    }
 
     return true;
   }

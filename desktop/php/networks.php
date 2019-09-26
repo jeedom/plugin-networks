4c945372cd04e3e45a6ed98ba75aec25fb793cc7
Loïc <loic@jeedom.com>
2019-09-26 09:21:48 +0200

    fix icon

--- desktop/php/networks.php
+++ desktop/php/networks.php
@@ -9,19 +9,19 @@ $eqLogics = eqLogic::byType($plugin->getId());
 
 
 <div class="row row-overflow">
-    <div class="col-xs-12 eqLogicThumbnailDisplay">
-		<legend><i class="icon loisir-two28"></i> {{Gestion}}</legend>
+	<div class="col-xs-12 eqLogicThumbnailDisplay">
+		<legend><i class="fas fa-cog"></i> {{Gestion}}</legend>
 		<div class="eqLogicThumbnailContainer">
 			<div class="cursor eqLogicAction logoPrimary" data-action="add" >
 				<i class="fas fa-plus-circle"></i>
 				<br/>
 				<span>{{Ajouter}}</span>
 			</div>
-            <div class="cursor eqLogicAction logoSecondary" data-action="gotoPluginConf">
-                <i class="fas fa-wrench"></i>
-                <br/>
-                <span>{{Configuration}}</span>
-            </div>
+			<div class="cursor eqLogicAction logoSecondary" data-action="gotoPluginConf">
+				<i class="fas fa-wrench"></i>
+				<br/>
+				<span>{{Configuration}}</span>
+			</div>
 		</div>
 		<legend><i class="fas fa-sitemap"></i> {{Mes Networks}}</legend>
 		<input class="form-control" placeholder="{{Rechercher}}" id="in_searchEqlogic" />
@@ -38,8 +38,8 @@ $eqLogics = eqLogic::byType($plugin->getId());
 			?>
 		</div>
 	</div>
-    
-    <div class="col-xs-12 eqLogic" style="display: none;">
+	
+	<div class="col-xs-12 eqLogic" style="display: none;">
 		<div class="input-group pull-right" style="display:inline-flex">
 			<span class="input-group-btn">
 				<a class="btn btn-default btn-sm eqLogicAction" data-action="configure"><i class="fas fa-cogs"></i> {{Configuration avancée}}</a><a class="btn btn-default btn-sm eqLogicAction" data-action="copy"><i class="fas fa-copy"></i> {{Dupliquer}}</a><a class="btn btn-sm btn-success eqLogicAction" data-action="save"><i class="fas fa-check-circle"></i> {{Sauvegarder}}</a><a class="btn btn-danger btn-sm eqLogicAction roundedRight" data-action="remove"><i class="fas fa-minus-circle"></i> {{Supprimer}}</a>
@@ -94,84 +94,85 @@ $eqLogics = eqLogic::byType($plugin->getId());
 								<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
 							</div>
 						</div>
-                        <br />
-                        <div class="form-group">
-                            <label class="col-sm-2 control-label">{{Adresse IP}}</label><div class="col-sm-3">
-                            <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="ip"/>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-sm-2 control-label">{{Adresse MAC (wol)}}</label>
-                            <div class="col-sm-3">
-                                <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="mac"/>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-sm-2 control-label">{{Broadcast IP (wol)}}</label>
-                            <div class="col-sm-3">
-                                <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="broadcastIP"/>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-sm-2 control-label">{{Méthode de ping}}</label>
-                            <div class="col-sm-3">
-                                <select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="pingMode">
-                                    <option value="ip">{{IP}}</option>
-                                    <option value="arp">{{ARP}}</option>
-                                    <option value="port">{{Port}}</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="form-group pingMode ip">
-                            <label class="col-sm-2 control-label">{{TTL}}</label>
-                            <div class="col-sm-3">
-                                <input type="number" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="ttl"/>
-                            </div>
-                        </div>
-                        <div class="form-group pingMode port">
-                            <label class="col-sm-2 control-label">{{Port}}</label>
-                            <div class="col-sm-3">
-                                <input type="number" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="port"/>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-sm-2 control-label">{{Auto-actualisation (cron)}}</label>
-                            <div class="col-sm-2">
-                                <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="autorefresh" placeholder="{{Auto-actualisation (cron)}}"/>
-                            </div>
-                            <div class="col-sm-1">
-                                <i class="fa fa-question-circle cursor floatright" id="bt_cronGenerator"></i>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-sm-2 control-label">{{Notifier si le ping est KO}}</label>
-                            <div class="col-sm-2">
-                                <input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="notifyifko"/>
-                            </div>
-                        </div>
-                    </fieldset>
-                </form>
-            </div>
-            <div role="tabpanel" class="tab-pane" id="commandtab">
+						<br />
+						<div class="form-group">
+							<label class="col-sm-2 control-label">{{Adresse IP}}</label><div class="col-sm-3">
+								<input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="ip"/>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-sm-2 control-label">{{Adresse MAC (wol)}}</label>
+							<div class="col-sm-3">
+								<input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="mac"/>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-sm-2 control-label">{{Broadcast IP (wol)}}</label>
+							<div class="col-sm-3">
+								<input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="broadcastIP"/>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-sm-2 control-label">{{Méthode de ping}}</label>
+							<div class="col-sm-3">
+								<select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="pingMode">
+									<option value="ip">{{IP}}</option>
+									<option value="arp">{{ARP}}</option>
+									<option value="port">{{Port}}</option>
+								</select>
+							</div>
+						</div>
+						<div class="form-group pingMode ip">
+							<label class="col-sm-2 control-label">{{TTL}}</label>
+							<div class="col-sm-3">
+								<input type="number" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="ttl"/>
+							</div>
+						</div>
+						<div class="form-group pingMode port">
+							<label class="col-sm-2 control-label">{{Port}}</label>
+							<div class="col-sm-3">
+								<input type="number" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="port"/>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-sm-2 control-label">{{Auto-actualisation (cron)}}</label>
+							<div class="col-sm-2">
+								<input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="autorefresh" placeholder="{{Auto-actualisation (cron)}}"/>
+							</div>
+							<div class="col-sm-1">
+								<i class="fa fa-question-circle cursor floatright" id="bt_cronGenerator"></i>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-sm-2 control-label">{{Notifier si le ping est KO}}</label>
+							<div class="col-sm-2">
+								<input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="notifyifko"/>
+							</div>
+						</div>
+					</fieldset>
+				</form>
+			</div>
+			<div role="tabpanel" class="tab-pane" id="commandtab">
 				<br/><br/>
 				<table id="table_cmd" class="table table-bordered table-condensed">
 					<thead>
 						<tr>
 							<th style="width: 250px;">{{Nom}}</th>
 							<th style="width: 80px;">{{Type}}</th>
-                            <th style="width: 80px;">{{Action}}</th>
+							<th style="width: 80px;">{{Action}}</th>
 						</tr>
 					</thead>
-
+					
 					<tbody>
 						
 					</tbody>
 				</table>
 				
 			</div>
-        
-    </div>  
-</div>
-
-<?php include_file('desktop', 'networks', 'js', 'networks');?>
-<?php include_file('core', 'plugin.template', 'js');?>
+			
+		</div>
+	</div>
+	
+	<?php include_file('desktop', 'networks', 'js', 'networks');?>
+	<?php include_file('core', 'plugin.template', 'js');?>
+	
